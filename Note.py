2af5ccfe3fc182ab9d6b5681e50f2a9956796172
sagarmshahh <67440686+sagarmshahh@users.noreply.github.com>
2021-01-31 23:10:51 +0530

    first commit

---
+++ Note.py
@@ -0,0 +1,22 @@
+import time
+
+note=' '
+note_file=''
+while note!='':
+    note=input()
+    note_file+=note+'\n'
+Time=['created on ',time.asctime(),' \n']
+
+fo=open('file.doc','a')  
+fo.writelines(Time)
+fo.writelines(note_file)
+fo.close()
+
+f=open('file.doc','r')
+File_info=f.read()
+print(File_info)
+
+f.close()
+
+
+
